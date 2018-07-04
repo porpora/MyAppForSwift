7810076e31182754f321133518cbf236c81f6705
porpora <pr_mn.r14_tea@i.softbank.jp>
2018-07-04 11:40:22 +0900

    Initial Commit

---
+++ MyAppTests/MyAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyAppTests.swift
+//  MyAppTests
+//
+//  Created by 宮内諒太 on 2018/05/28.
+//  Copyright © 2018年 宮内諒太. All rights reserved.
+//
+
+import XCTest
+@testable import MyApp
+
+class MyAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
